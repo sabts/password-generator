e92e198cfd8e559d91e54be50f547b032a61d371
sabts <sabrinataraschi@gmail.com>
2025-05-09 19:10:13 +0200

    .

--- src/components/password-generator/PasswordGenerator.jsx
+++ src/components/password-generator/PasswordGenerator.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
+import {PASSWORD_CONTENT} from "../../constants/passwordContent"
 
 const PasswordGenerator = () =>{
-const [rangeValue, setRangeValue] = useState(0)
+const [rangeValue, setRangeValue] = useState(4);
+const [includeUppercase, setIncludeUppercase] = useState(false);
+const [includeLowercase, setIncludeLowercase] = useState(false);
+const [includeNumbers, setIncludeNumbers] = useState(false);
+const [includeSymbols, setIncludeSymbols] = useState(false);
+const allCheckboxs = [includeUppercase, includeLowercase, includeNumbers, includeSymbols];
+//value={password} 
+//const password = generateAleatoryCharacters
 
     return <>
     <div>
         <label>
-        <input type="text"/>
+        <input type="text" placeholder="P4$W0RD!" readOnly/>
         </label>
     </div>
     <div>
@@ -17,32 +25,40 @@ const [rangeValue, setRangeValue] = useState(0)
     <div> 
         <span>Include Uppercase</span>
         <label>
-        <input type="checkbox"/>
+        <input type="checkbox"
+         checked={includeUppercase}
+         onChange={() => setIncludeUppercase(!includeUppercase)}/>
         </label>
         </div>
        
     <div>
         <span>Include Lowercase</span>
         <label>
-        <input type="checkbox"/>
+        <input type="checkbox"
+        checked={includeLowercase}
+        onChange={() => setIncludeLowercase(!includeLowercase)}/>
         </label> 
     </div>
            
     <div>  
         <span>Include Numbers</span>
         <label>
-        <input type="checkbox"/>
+        <input type="checkbox"
+        checked={includeNumbers}
+        onChange={() => setIncludeNumbers(!includeNumbers)}/>
         </label>
     </div>
 
     <div> 
         <span>Include Symbols</span>
         <label>
-        <input type="checkbox"/>
+        <input type="checkbox"
+        checked={includeSymbols}
+        onChange={() => setIncludeSymbols(!includeSymbols)}/>
         </label> 
         </div>
 
-    <button disabled> Generate Password </button>
+    <button disabled={!toggleCheekerActivateButton(allCheckboxs)}> Generate Password </button>
     </>
 }
 export default PasswordGenerator
@@ -51,3 +67,11 @@ const showRangeValue =  (event, setRangeValue)=> {
     const newValue = event.target.value
     setRangeValue(newValue)
 }
+
+const toggleCheekerActivateButton = (allCheckboxs) => {
+    return allCheckboxs.some(check => check)
+}
+
+const generateAleatoryCharacters = max => {
+    return Math.floor(Math.random() * max);
+  };
---
+++ src/constants/passwordContent.js
@@ -0,0 +1,6 @@
+export const PASSWORD_CONTENT = {
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercase: "abcdefghijklmnopqrstuvwxyz/",
+    numbers: "01234567890",
+    symbols: "!@#$%^&*()_+-={}[]:;<>,.?/",
+  };
